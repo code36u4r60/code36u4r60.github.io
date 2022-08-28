8489b15132ea2e79631f2006c319f11ddb23b0cd
Eduardo Queiros <code36u4r60@hotmmail.com>
2022-08-28 14:14:35 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
-    outDir: 'docs'
+  build: {
+    outDir: 'docs',
   },
-  plugins: [react()]
-
+  plugins: [react()],
 })
